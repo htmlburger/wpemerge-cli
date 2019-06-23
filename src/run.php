c299564b07eeb56a6840285374e8d262cd0cb978
Atanas Angelov <hi@atanas.dev>
2019-06-23 22:40:45 +0300

    Add runner.php so it can be run directly as well without relying on shell #!

---
+++ src/run.php
@@ -0,0 +1,19 @@
+<?php
+if ( php_sapi_name() !== 'cli' ) {
+	exit;
+}
+
+$autoloaders = [
+	dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+	dirname( dirname( dirname( __DIR__ ) ) ) . DIRECTORY_SEPARATOR . 'autoload.php',
+	dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+];
+
+foreach ( $autoloaders as $autoloader ) {
+	if ( file_exists( $autoloader ) ) {
+		require_once $autoloader;
+		break;
+	}
+}
+
+\WPEmerge\Cli\App::run();
