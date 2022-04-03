a06821fad5292d53d37e9e08e218e84067106b4f
Atanas Angelov <hi@atanas.dev>
2022-04-04 01:04:07 +0300

    Update Tailwind CSS to v3

--- src/Presets/TailwindCss.php
+++ src/Presets/TailwindCss.php
@@ -3,7 +3,6 @@
 namespace WPEmerge\Cli\Presets;
 
 use Symfony\Component\Console\Output\OutputInterface;
-use WPEmerge\Cli\App;
 
 class TailwindCss implements PresetInterface {
 	use FrontEndPresetTrait;
@@ -20,15 +19,20 @@ class TailwindCss implements PresetInterface {
 	 * {@inheritDoc}
 	 */
 	public function execute( $directory, OutputInterface $output ) {
-		$this->installNodePackage( $directory, $output, 'tailwindcss', '^2' );
+		$this->installNodePackage( $directory, $output, 'tailwindcss', '^3' );
 
 		$this->copy([
 			$this->path( WPEMERGE_CLI_DIR, 'src', 'TailwindCss', 'tailwindcss.scss' )
 			=> $this->path( $directory, 'resources', 'styles', 'frontend', 'vendor', 'tailwindcss.scss' ),
+			$this->path( WPEMERGE_CLI_DIR, 'src', 'TailwindCss', 'tailwindcss.js' )
+			=> $this->path( $directory, 'resources', 'build', 'tailwindcss.js' ),
 		]);
 
 		$postcss_js_filepath = $this->path( $directory, 'resources', 'build', 'postcss.js' );
 		$this->enablePreset( $postcss_js_filepath, 'Tailwind CSS' );
+
+		$development_mode_plugin_js_filepath = $this->path( $directory, 'resources', 'build', 'lib', 'development-mode-plugin.js' );
+		$this->enablePreset( $development_mode_plugin_js_filepath, 'Tailwind CSS' );
 
 		$components_dir = $this->path( $directory, 'resources', 'styles', 'frontend', 'components' );
 		if ( ! file_exists( $components_dir ) ) {
@@ -38,12 +42,6 @@ class TailwindCss implements PresetInterface {
 		$utilities_dir = $this->path( $directory, 'resources', 'styles', 'frontend', 'utilities' );
 		if ( ! file_exists( $utilities_dir ) ) {
 			mkdir( $utilities_dir );
-		}
-
-		$tailwind_bin_filepath = $this->path( $directory, 'node_modules', '.bin', 'tailwind' );
-		$tailwind_js_filepath = $this->path( $directory, 'resources', 'build', 'tailwindcss.js' );
-		if ( ! file_exists( $tailwind_js_filepath ) ) {
-			App::execute( [$tailwind_bin_filepath, 'init', $tailwind_js_filepath, $directory] );
 		}
 	}
 }
